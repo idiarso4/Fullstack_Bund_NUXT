cdc6e168b405c7b7b8fda1bad1d3575b4ebf05bc
idiarso4 <idiarso48@guru.smk.belajar.id>
2025-08-03 23:16:32 +0700

    initial commit

---
+++ apps/web/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: 'latest',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/ui'
+    ,
+    'convex-nuxt'
+  ],
+  css: ['~/assets/css/main.css'],
+  devServer: {
+    port: 3001
+  },
+  ssr: false,
+  convex: {
+    url: process.env.NUXT_PUBLIC_CONVEX_URL,
+  },
+  runtimeConfig: {
+    public: {
+      serverURL: process.env.NUXT_PUBLIC_SERVER_URL,
+    }
+  }
+})
